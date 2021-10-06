2db9fe644e9cbb72cd6fc8e1530d376d9ca52c4d
dpessanha <daopessanha@gmail.com>
2021-10-06 14:29:13 -0300

    edit route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const override = require('method-override')
 const Campground = require('./models/campground')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -16,6 +17,12 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
+app.use(override('_method'))
+
+// =====================
+//   LANDING PAGE ROUTE
+// =====================  
 app.get('/', (req, res) => {
   res.render('home')
 })
@@ -25,7 +32,19 @@ app.get('/', (req, res) => {
 // =====================  
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({})
-  res.render('campgrounds/index', { title: 'Campgrounds', campgrounds })
+  res.render('campgrounds/index', { title: 'YelpCamp | Campgrounds', campgrounds })
+})
+
+// =====================
+//   CREATE ROUTE
+// =====================  
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new', { title: 'YelpCamp | New Campground' })
+})
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground)
+  await campground.save()
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 
 // =====================
@@ -34,12 +53,26 @@ app.get('/campgrounds', async (req, res) => {
 app.get('/campgrounds/:id', async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findById(id)
-  res.render('campgrounds/details', { title: `${campground.title}`, campground })
+  res.render('campgrounds/details', { title: `YelpCamp | ${campground.title}`, campground })
+})
+
+// =====================
+//   EDIT ROUTE
+// =====================  
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findById(id)
+  res.render('campgrounds/edit', { title: `YelpCamp | Edit ${campground.title}`, campground })
+})
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 
 
 
-
+/////////// Server Start
 app.listen(3000, () => {
   console.log('Server running at port 3000.');
 })
