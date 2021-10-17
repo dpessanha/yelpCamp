19477eb13a4202a46ae117a8a86d93ad34e2d4b6
dpessanha <daopessanha@gmail.com>
2021-10-17 11:28:32 -0300

    Validations and errors

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 const ejsMate = require('ejs-mate')
 const override = require('method-override')
-const Campground = require('./models/campground')
 const mongoose = require('mongoose')
+const { campgroundSchema } = require('./joi/schemas')
+const Campground = require('./models/campground')
 const ExpressError = require('./utils/ExpressError')
+const catchAsync = require('./utils/catchAsync')
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -25,6 +28,20 @@ app.use(express.urlencoded({ extended: true }))
 app.use(override('_method'))
 
 // =====================
+//   MIDDLEWARES
+// =====================  
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body)
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else{
+    next()
+   }
+}
+
+
+// =====================
 //   LANDING PAGE ROUTE
 // =====================  
 app.get('/', (req, res) => {
@@ -34,10 +51,10 @@ app.get('/', (req, res) => {
 // =====================
 //   INDEX ROUTE
 // =====================  
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
   const campgrounds = await Campground.find({})
   res.render('campgrounds/index', { title: 'YelpCamp | Campgrounds', campgrounds })
-})
+}))
 
 // =====================
 //   CREATE ROUTE
@@ -45,47 +62,56 @@ app.get('/campgrounds', async (req, res) => {
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new', { title: 'YelpCamp | New Campground' })
 })
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+  // if(!req.body.campground) throw new ExpressError(400, 'Invalid campground Data.')
   const campground = new Campground(req.body.campground)
   await campground.save()
   res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // =====================
 //   DETAILS ROUTE
 // =====================  
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findById(id)
   res.render('campgrounds/details', { title: `YelpCamp | ${campground.title}`, campground })
-})
+}))
 
 // =====================
 //   EDIT ROUTE
 // =====================  
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findById(id)
   res.render('campgrounds/edit', { title: `YelpCamp | Edit ${campground.title}`, campground })
-})
-app.put('/campgrounds/:id', async (req, res) => {
+}))
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
   res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // =====================
-//   EDIT ROUTE
+//   DELETE ROUTE
 // =====================
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findByIdAndDelete(id)
   res.redirect('/campgrounds')
+}))
+
+// =====================
+//   ERROR HANDLING
+// =====================
+app.all('*', (re, res, next) => {
+  next(new ExpressError('Page Not Found.', 404))
 })
 
-// Error handling
-app.use((err, req, res, next)=> {
-  res.send(`There is an error in the request.`)
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err
+  if(!err.message) err.message = 'Something went wrong...'
+  res.status(statusCode).render('error', { err, title: 'Error!' })
 })
 
 /////////// Server Start
